f50de4e89aa9032e6c8135c60f4d6a2e1e98f532
davidlab44 <davidlab44@gmail.com>
2023-08-15 22:52:58 -0500

    frank-failed

---
+++ app/src/main/java/com/david/tot/data/network/reloadable/IReloadableApiClient.kt
@@ -0,0 +1,44 @@
+package com.david.tot.data.network.reloadable
+
+import com.david.tot.domain.model.Article
+import com.david.tot.domain.model.Reloadable
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.*
+//import retrofit2.Retrofit
+
+interface IReloadableApiClient {
+    /*@GET("products")
+    suspend fun getAllRecipes(): Response<List<Article>>
+    //api/Article
+    */
+
+    @GET("api/APP_SP_DrugsDeliveryConsumerViewArticlesResult")
+    suspend fun getAll(): Response<List<Reloadable>>
+
+    @POST("products")
+    suspend fun addOne(@Body product: Reloadable): Response<ResponseBody>
+
+    @POST("api/GlappDrugsDeliveryConsumptionDets")
+    suspend fun postMany(@Body jsonArray: RequestBody): Response<ResponseBody>
+
+    @PUT("products/{id}")
+    suspend fun updateOne(@Path("id") id:String, @Body product:Article): Response<ResponseBody>
+
+    @DELETE("products/{id}")
+    suspend fun deleteOne(@Path("id") id:Int): Response<ResponseBody>
+
+    @Multipart
+    //@POST("pictures")
+    @POST("api/FileManager")
+    @JvmSuppressWildcards
+    //suspend fun uploadPicture(@Part part: MultipartBody.part): Response<ResponseBody>
+    suspend fun uploadPicture(@Part part: MultipartBody.Part,@PartMap params: Map<String,RequestBody>): Response<ResponseBody>
+
+    //suspend fun uploadPicture(@Part part: MultipartBody.Part,@Field("AltText") AltText:String?,@Field("Description") Description:String?): Response<ResponseBody>
+    //suspend fun uploadPicture(@Part part: MultipartBody.Part,@Part("AltText") AltText:String,@Part("Description") Description:String): Response<ResponseBody>
+    //suspend fun uploadPicture(@Part part: MultipartBody.Part): Response<ResponseBody>
+}
+
